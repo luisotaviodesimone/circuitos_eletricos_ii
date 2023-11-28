9be970bc358001bacc5da09d9451e1b9c9bc595a
luisotaviodesimone <85963351+luisotaviodesimone@users.noreply.github.com>
2023-11-28 10:52:30 -0300

    feat: include pytest lib

--- trab3/test_testing.py
+++ trab3/test_testing.py
@@ -54,47 +54,48 @@ def compareDC(execution, result):
         color(f"{np.allclose(execution, result, rtol=1e-03, atol=1e-03)}", "blue"),
         "\n",
     )
+    assert np.allclose(execution, result, rtol=1e-03, atol=1e-03)
 
 
-def continuous_current():
-    print("teste1.txt")
+def test_continuous_current():
+    print(color("teste1.txt", "green"))
     compareDC(
         executeDC("teste1.txt", "DC", [1, 2], [1e-10, [0, 0.1, 0.1]]),
         [2.0, 1.30491003],
     )
 
     # Does not converge
-    print("teste2.txt")
+    print(color("teste2.txt", "green"))
     compareDC(executeDC("teste2.txt", "DC", [2], [1e-14, [0, 3, 3]]), [0.73068013])
 
     # Does not converge
-    print("teste3.txt")
+    print(color("teste3.txt", "green"))
     compareDC(executeDC("teste3.txt", "DC", [2], [1e-12, [0, 0, 2]]), [2])
 
-    print("teste4.txt")
+    print(color("teste4.txt", "green"))
     compareDC(executeDC("teste4.txt", "DC", [2], [1e-12, [0, 0, 2]]), [1.9])
 
-    print("teste5.txt")
+    print(color("teste5.txt", "green"))
     compareDC(executeDC("teste5.txt", "DC", [1, 2], [1e-12, [0, 1, 2]]), [5, 4.2781936])
 
-    print("teste6.txt")
+    print(color("teste6.txt", "green"))
     compareDC(
         executeDC("teste6.txt", "DC", [1, 2, 3], [1e-14, [0, 5, 1, -5]]),
         [5, 4.2781936, -5],
     )
 
-    print("teste7.txt")
+    print(color("teste7.txt", "green"))
     compareDC(
         executeDC("teste7.txt", "DC", [1, 2, 3], [1e-14, [0, -5, 4, 5]]),
         [-5, 4.2781936, 5],
     )
 
 
-continuous_current()
+test_continuous_current()
 
 
 def transient_regime():
-    print("teste1.txt")
+    print(color("teste1.txt", "green"))
     executeTRAN("teste1.txt", "DC", [1, 2], [1e-10, [0, 0.1, 0.1]])
 
 
